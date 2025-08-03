7aa7daece47bb328f664e719b4db8754f3af8a28
shibata keigo <piyomame.7k@gmail.com>
2025-08-03 16:20:58 +0900

    名前の共有

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 
 import streamlit as st
 
-st.title("名前記憶アプリ")
+st.title("ユーザー情報入力")
 
-#session_stateで名前を記憶
+#session_stateの初期化
 if 'name' not in st.session_state:
-    st.session_state.name = ""
+    st.session_state.user_name = ""
 
+#ユーザー名の入力
 name = st.text_input("あなたの名前を入力してください")
-if st.button("名前を記憶"):
-    st.session_state.name = name
+if st.button("名前を保存"):
+    st.session_state.user_name = name
+    st.success("名前を保存しました!")
 
-st.write(f"記憶している名前：{st.session_state.name}")
+st.write(f"現在保存されている名前：{st.session_state.user_name}")
